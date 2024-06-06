dfbb0a2f79f1d60af6c7a435dbb7f888afb45642
DoomaticSudds <109631397+DoomaticSudds@users.noreply.github.com>
2024-06-06 16:26:58 +0000

    Completed requiredments for Platformer.js, althought the game itself is not up to expectation

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -44,16 +44,30 @@ $(function () {
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
 
+    createPlatform(750, 650, 150, 20);
+    createPlatform(955, 250, 250, 20);     // The one that get shots by top
+    createPlatform(400, 560, 200, 20);  // Second Jump
+    createPlatform(675, 450, 100, 50); // Big Block
+    createPlatform(900, 385, 100, 20); // by Big block on right side
+    createPlatform(250, 650, 50, 10); // First block
+    createPlatform(150, 555, 50, 10); // Alt First?
+    createPlatform(300, 475, 95, 10); // Second jump for alt first
 
 
-    
+
+
+
+
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
 
-
+    createCollectable("steve", 495, 500, 5, 0.1); // Second Jump
+    createCollectable("diamond", 750, 568, 3, 0.75); // Big block
+    createCollectable("database", 900, 125, 1, 0.75); // Stair 4
+  
 
 
     // TODO 3
@@ -61,7 +75,12 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
+// cannon on left wall, 600px down, shooting once per second
 
+
+    createCannon("left", 575, 1755);
+    createCannon("bottom", 450, 1050);  
+    createCannon("top", 875, 1050);  
 
 
 
