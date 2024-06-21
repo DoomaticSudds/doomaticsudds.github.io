32acbe47837595ae2897ab7cf30d3e713f8bc9fa
DoomaticSudds <109631397+DoomaticSudds@users.noreply.github.com>
2024-06-21 16:28:47 +0000

    Final Functionality Commit, added illiteration to make all circles in project move. Last push/commit will be done to add design decsions to project. - Sudds

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -59,23 +59,62 @@ var init = function (window) {
         function update() {
             // TODO 4 : Update the circle's position //
 
-            
-    
+            for (var i = 0; i < circles.length; i++){
+                physikz.updatePosition(circles[i]);
+            }
+
+
+            /*
+
             physikz.updatePosition(circles[0]);
             physikz.updatePosition(circles[1]);
             physikz.updatePosition(circles[2]);
             physikz.updatePosition(circles[3]);
             physikz.updatePosition(circles[4]);
+            physikz.updatePosition(circles[5]);
+            physikz.updatePosition(circles[6]);
+            physikz.updatePosition(circles[7]);
+            physikz.updatePosition(circles[8]);
+            physikz.updatePosition(circles[9]);
+            physikz.updatePosition(circles[10]);
+
+            */
+
+
+
+
+        
+
 
 
 
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+          
+            for (var i = 0; i < circles.length; i++){
+                game.checkCirclePosition(circles[i]);
+            }
+
+
+            /*
             game.checkCirclePosition(circles[0]);
             game.checkCirclePosition(circles[1]);
             game.checkCirclePosition(circles[2]);
             game.checkCirclePosition(circles[3]);
             game.checkCirclePosition(circles[4]);
+            game.checkCirclePosition(circles[5]);
+            game.checkCirclePosition(circles[6]);
+            game.checkCirclePosition(circles[7]);
+            game.checkCirclePosition(circles[8]);
+            game.checkCirclePosition(circles[9]);
+            game.checkCirclePosition(circles[10]);
+           
+            */
+
+
+
+
+
+
 
             // TODO 9 : Iterate over the array
            
